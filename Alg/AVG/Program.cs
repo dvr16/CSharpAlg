e5c7b139fa10a66d4762cf75d4afdce3bcd25347
Дмитрий <dvr07@yandex.ru>
2021-03-15 20:52:46 +0300

    Среднее арифметическое значение двух чисел

---
+++ Alg/AVG/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AVG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstName, secondName;
+
+            Console.Write("Введите первое число: ");
+            firstName = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            secondName = double.Parse(Console.ReadLine());
+
+            double result = (firstName + secondName) / 2;
+            Console.WriteLine($"Среднее арифметическое значение равно {result}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
