aedcf5afafddfc31b1657ffd263ca486611ec6f8
Дмитрий <dvr07@yandex.ru>
2021-03-16 19:54:19 +0300

    Ввести три числа и вывести их сумму и произведение

---
+++ Alg/SumAndMultiplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumAndMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first, second, third;
+
+            Console.Write("Введите первое число: ");
+            first = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            second = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите третье число: ");
+            third = double.Parse(Console.ReadLine());
+
+            double sum = first + second + third;
+            double mult = first * second * third;
+
+            Console.WriteLine($"Сумма трех этих чисел равно {sum}");
+            Console.WriteLine($"Произведение трех этих чисел равно {mult}");
+
+            Console.ReadLine();
+        }
+    }
+}
