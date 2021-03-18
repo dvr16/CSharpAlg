e6feb6aec69cde5492e3b96931f66066c9535e9c
Дмитрий <dvr07@yandex.ru>
2021-03-18 19:39:58 +0300

    Проверка числа на четность

---
+++ Alg/EvenAndOdd/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EvenAndOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.Write("Введите целое число: ");
+
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("число четное");
+            }
+            else
+            {
+                Console.WriteLine("число нечетное");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
