2848ab3de3c5725f0a050686e55485432fad7d54
Дмитрий <dvr07@yandex.ru>
2021-03-23 20:27:58 +0300

    Калькулятор

---
+++ Alg/Calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double firstValue = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double secondValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+
+            string action;
+            action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine($"Сумма этих чисел равна {firstValue + secondValue}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Разность этих чисел равна {firstValue - secondValue}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Произведение этих чисел равна {firstValue * secondValue}");
+                    break;
+                case "/":
+                    Console.WriteLine($"Частное этих чисел равна {firstValue / secondValue}");
+                    break;
+                default:
+                    Console.WriteLine("Такой операции не существует!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
