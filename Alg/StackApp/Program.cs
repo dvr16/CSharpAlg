ee1a1cd5ce427e994d020b668d21df7f36b19e68
Дмитрий <dvr07@yandex.ru>
2021-03-29 12:21:14 +0300

    Реализация стека

---
+++ Alg/StackApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackX theStack = new StackX(10); // Создание нового стека
+            theStack.push(20);      // Занесение элементов в стек
+            theStack.push(40);
+            theStack.push(60);
+            theStack.push(80);
+
+            while (!theStack.isEmpty()) // Пока стек не станет пустым
+            {                           // Удалить элемент из стека
+                long value = theStack.pop();
+                Console.Write(value);   // Вывод содержимого
+                Console.Write(" ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ Alg/StackApp/StackX.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackApp
+{
+    class StackX
+    {
+        private int maxSize;       // Размер массива
+        private long[] stackArray;
+        private int top;           // Вершина стека
+
+        public StackX(int s)       // Конструктор
+        {
+            maxSize = s;           // Определение размера стека
+            stackArray = new long[maxSize]; // Создание массива
+            top = -1;              // Пока нет ни одного массива
+        }
+
+        public void push(long j)   // Размещение размера на вершине стека
+        {
+            stackArray[++top] = j; // Увеличение top, вставка элемента
+        }
+
+        public long pop()          // Извлечение элемента с вершины стека
+        {
+            return stackArray[top--];       // Извлечение элемента, уменьшение top
+        }
+
+        public long peek()         // Чтение элемента с вершины стека
+        {
+            return stackArray[top];
+        }
+
+        public Boolean isEmpty()   // True, если стек пуст
+        {
+            return (top == -1);
+        }
+
+        public Boolean isFull()    // True, если стек полон
+        {
+            return (top == maxSize - 1);
+        }
+    }
+}
